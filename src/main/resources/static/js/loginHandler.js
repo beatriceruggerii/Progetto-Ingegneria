216718f3a97b0eb23a9fb46a468df05eac02e926
thomasticato <mazzocchi.thomas02@gmail.com>
2024-09-23 16:14:11 +0200

    Login and Registration front-end

---
+++ src/main/resources/static/js/loginHandler.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    document.getElementById('login').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const usernameValue = document.getElementById('uname').value;
+        const passwordValue = document.getElementById('pword').value;
+
+        const user = {
+            username: usernameValue,
+            password: passwordValue,
+            email: null
+        };
+        console.log(user);
+
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Se la richiesta è andata a buon fine, puoi fare qualcosa, ad esempio reindirizzare l'utente a un'altra pagina
+                    window.location.href = 'homepage.html';
+                } else {
+                    $('#credenzialiErrate').modal('show'); // Mostra il modal
+                    throw new Error('Errore nella richiesta');
+                }
+            })
+            .catch(error => {
+                console.error.name('Si è verificato un errore:', error);
+            });
+    });
+
+});
