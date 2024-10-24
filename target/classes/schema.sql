85379fe1fe96a19ebf0f5bea9d96593f085091a5
matildez03 <98044488+matildez03@users.noreply.github.com>
2024-10-24 12:57:45 +0200

    target

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,3 +1,11 @@
+DROP INDEX IF EXISTS unico_scenario_iniziale_per_storia;
+
 CREATE UNIQUE INDEX unico_scenario_iniziale_per_storia
     ON scenario (fk_storia)
     WHERE iniziale = TRUE;
+
+ALTER TABLE inventario DROP CONSTRAINT IF EXISTS fk_partita;
+
+-- Crea un nuovo vincolo di chiave esterna con ON DELETE CASCADE tra partita e inventario
+ALTER TABLE inventario
+    ADD CONSTRAINT fk_partita FOREIGN KEY (partita_id) REFERENCES partita(id) ON DELETE CASCADE;
