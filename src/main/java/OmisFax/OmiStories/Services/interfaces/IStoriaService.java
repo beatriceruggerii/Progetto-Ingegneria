fb067dc960295a65c1b1488be3dfc8e3133f77aa
leofr <fratoni.037@gmail.com>
2024-10-30 22:26:37 +0100

    pulizia codice

--- src/main/java/OmisFax/OmiStories/Services/interfaces/IStoriaService.java
+++ src/main/java/OmisFax/OmiStories/Services/interfaces/IStoriaService.java
@@ -1,14 +1,7 @@
 package OmisFax.OmiStories.Services.interfaces;
 
-import OmisFax.OmiStories.Entities.Scenario;
-import OmisFax.OmiStories.Entities.Scelta;
-import OmisFax.OmiStories.Entities.Indovinello;
-import OmisFax.OmiStories.Entities.Oggetto;
 import OmisFax.OmiStories.Entities.Storia;
 import OmisFax.OmiStories.DTOs.StoriaDTO;
-
-import java.util.List;
-import java.util.Map;
 
 public interface IStoriaService {
     Storia salvaStoria(StoriaDTO payload, String username);
